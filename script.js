6b2efd585ddcaf6124134a5d9c928e544ba7b46f
fojogrimmo <afanasyvekarindm@gmail.com>
2023-03-06 22:35:40 +0300

    Add game logic

---
+++ script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 100) + 1;
+let score = 10;
+let highscore = 0;
+let attempts = 0;
+let input = document.querySelector(".check__guess");
+
+const displayMessage = function (message) {
+  document.querySelector(".status__message").textContent = message;
+};
+
+document.querySelector(".button__check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".check__guess").value);
+  // Upadate attemps
+  attempts++;
+  document.querySelector(".attempts").textContent = attempts;
+
+  // When there is no input
+  if (!guess) {
+    displayMessage("No such number...");
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage("Correct number!");
+    document.querySelector(".header__number").textContent = secretNumber;
+    document.querySelector(".header__number").style.width = "35rem";
+    document.querySelector("body").style.backgroundColor = "#2e1f49";
+
+    // Upadate highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("You lost the game... Kitty won!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+// Again Button
+document.querySelector(".button__again").addEventListener("click", function () {
+  score = 10;
+  secretNumber = Math.trunc(Math.random() * 100) + 1;
+  document.querySelector(".score").textContent = score;
+  displayMessage("Start guessing...");
+  document.querySelector("body").style.backgroundColor = "#a66297";
+  document.querySelector(".header__number").style.width = "20rem";
+  document.querySelector(".header__number").textContent = "?";
+  document.querySelector(".check__guess").value = "";
+});
+
+// Add Enter to input
+input.addEventListener("keypress", function (press) {
+  if (press.key === "Enter") {
+    document.querySelector(".button__check").click();
+  }
+});
